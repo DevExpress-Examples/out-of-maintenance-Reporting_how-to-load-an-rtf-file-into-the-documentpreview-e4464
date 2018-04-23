e31d2209d703b9bf31a35692811139429d11335d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MinimalisticReportPreviewDemo/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Printing;
+using Microsoft.Win32;
+using System.IO;
+// ...
+
+namespace MinimalisticReportPreviewDemo {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            ThemeManager.ApplicationThemeName = "Office2007Silver";
+            InitializeComponent();
+        }
+
+        private void ShowPreview(object sender, RoutedEventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true) {
+                Report report = new Report();
+                report.xrRichText1.Rtf = File.ReadAllText(ofd.FileName);
+                XtraReportPreviewModel model = new XtraReportPreviewModel(report);
+                DocumentPreviewWindow window = new DocumentPreviewWindow() { Model = model };
+                report.CreateDocument(true);
+                window.ShowDialog();
+            }
+        }
+
+    }
+}
---
+++ VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Windows
+Imports DevExpress.Xpf.Core
+Imports DevExpress.Xpf.Printing
+Imports Microsoft.Win32
+Imports System.IO
+' ...
+
+Namespace MinimalisticReportPreviewDemo
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			ThemeManager.ApplicationThemeName = "Office2007Silver"
+			InitializeComponent()
+		End Sub
+
+		Private Sub ShowPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim ofd As New OpenFileDialog()
+			If ofd.ShowDialog() = True Then
+				Dim report As New Report()
+				report.xrRichText1.Rtf = File.ReadAllText(ofd.FileName)
+				Dim model As New XtraReportPreviewModel(report)
+				Dim window As New DocumentPreviewWindow() With {.Model = model}
+				report.CreateDocument(True)
+				window.ShowDialog()
+			End If
+		End Sub
+
+	End Class
+End Namespace
