8a6ba03bd6e747ca42fc1ebadc19b758b5f70d05
DevExpressExampleBot <fakeEmail>
2021-09-02 19:45:28 +0300

    Source auto update [skip ci]

--- VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
+++ VB/MinimalisticReportPreviewDemo/MainWindow.xaml.vb
@@ -6,29 +6,29 @@ Imports Microsoft.Win32
 ' ...
 
 Namespace MinimalisticReportPreviewDemo
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
-    Partial Public Class MainWindow
-        Inherits Window
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
 
-        Public Sub New()
-            ApplicationThemeHelper.ApplicationThemeName = "Office2007Silver"
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			ApplicationThemeHelper.ApplicationThemeName = "Office2007Silver"
+			InitializeComponent()
+		End Sub
 
-        Private Sub ShowPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            Dim ofd As New OpenFileDialog()
-            If ofd.ShowDialog() = True Then
-                Dim report As New Report()
-                report.xrRichText1.Rtf = File.ReadAllText(ofd.FileName)
+		Private Sub ShowPreview(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			Dim ofd As New OpenFileDialog()
+			If ofd.ShowDialog().Equals(True) Then
+				Dim report As New Report()
+				report.xrRichText1.Rtf = File.ReadAllText(ofd.FileName)
 
-                Dim window As New DocumentPreviewWindow()
-                window.PreviewControl.DocumentSource = report
-                report.CreateDocument(True)
-                window.ShowDialog()
-            End If
-        End Sub
+				Dim window As New DocumentPreviewWindow()
+				window.PreviewControl.DocumentSource = report
+				report.CreateDocument(True)
+				window.ShowDialog()
+			End If
+		End Sub
 
-    End Class
+	End Class
 End Namespace
